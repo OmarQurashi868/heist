2a89338773e7e61073972c849783785a34d2aca4
Akram <tubeakram7@gmail.com>
2023-10-03 11:55:50 +0300

    Added money bag and follow

--- object_scenes/money_bag.gd
+++ object_scenes/money_bag.gd
@@ -1,10 +1,26 @@
 extends Area3D
 
+@onready var game_manager: Node3D = $"../game_manager"
+@onready var slot: Node3D = $"../Player/money_bag_slot"
+var is_picked_up = false
+var angle = 0
+@export var amp = 0.5
+@export var freq = 0.5
+@export var speed = 6
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	if is_picked_up:
+		position.x = move_toward(position.x, slot.global_position.x, speed * delta)
+		position.y = move_toward(position.y, slot.global_position.y, speed * delta)
+		position.z = move_toward(position.z, slot.global_position.z, speed * delta)
+	else:
+		angle += 2 * 3.14 * delta
+		position.y = sin(angle * freq) * amp + 2
+		
 
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		game_manager.grab_bag(0)
+	
--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
+@onready var money_bag: Area3D = $"../money_bag"
+@onready var player: CharacterBody3D = $"../Player"
 var team_scores = {"red": 0, "blue": 0, "green": 0, "yellow": 0}
 
 func add_score(team):
 	team_scores[team] += 1
 	print(team_scores)
+
+func grab_bag(player_id):
+	money_bag.is_picked_up = true
+	
+	
