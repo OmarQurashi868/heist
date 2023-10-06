984df852873c54820ebd6f588aeeb5125c2f4391
Akram <tubeakram7@gmail.com>
2023-10-06 05:12:12 +0300

    tried to get the 4 spawns positions

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -4,6 +4,11 @@ extends Node3D
 # @onready var player: CharacterBody3D = $"../Player"
 var team_scores = {"red": 0, "blue": 0, "green": 0, "yellow": 0}
 var team_order = ["red", "blue", "green", "yellow" ]
+@onready var team_red_spawn_position = get_node("../level_1/team_red_spawn").position
+@onready var team_blue_spawn_position = get_node("../level_1/team_blue_spawn").position
+@onready var team_green_spawn_position = get_node("../level_1/team_green_spawn").position
+@onready var team_yellow_spawn_position = get_node("../level_1/team_yellow_spawn").position
+var team_spawns = [team_red_spawn_position, team_blue_spawn_position, team_green_spawn_position, team_yellow_spawn_position]
 @export var players_num = 4
 @export var player_scene: PackedScene = preload("res://object_scenes/player.tscn")
 
@@ -13,7 +18,9 @@ func spawn_player():
 		plr.name += str(i+1)
 		get_parent().add_child.call_deferred(plr)
 		plr.add_to_group(team_order[i])
-		print(plr.get_groups())
+		#var players = get_tree().get_nodes_in_group(team_order[i])
+		#players.global_position = team_spawns[i]
+		print(team_spawns[i])
 	
 
 func _ready():
