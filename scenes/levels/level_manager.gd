2c195f30b4e6195b2ac8ea360f81768e0b65f2ec
PsychonicJoe <51345357+PsychonicJoe@users.noreply.github.com>
2023-10-06 04:01:25 +0400

    idk man

--- scenes/levels/level_manager.gd
+++ scenes/levels/level_manager.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @onready var game_manager: Node3D = $"../../game_manager"
+signal baseTouched
 
 func _on_base_red_body_entered(body):
+	baseTouched.emit()
 	if body.is_in_group("player") and body.is_in_group("red"):
 		game_manager.touch_base(body.player_id, "red")
 
