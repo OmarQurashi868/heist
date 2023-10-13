7c6ccd24d51adbcfd0a6cf3342ab4690bd3506e5
PsychonicJoe <51345357+PsychonicJoe@users.noreply.github.com>
2023-10-14 01:39:28 +0400

    fixed shadowed var names

--- classes/attack.gd
+++ classes/attack.gd
@@ -6,8 +6,8 @@ var knockback_force: float
 var knockback_source: Vector3
 var stun_timer: float
 
-func _init(damage = 0, knock_force = 0, knockback_source = Vector3.ZERO, stun_timer = 0):
-	damage = damage
-	knockback_force = knockback_force
-	knockback_source = knockback_source
-	stun_timer = stun_timer
+func _init(given_damage = 0, given_knockback_force = 0, given_knockback_source = Vector3.ZERO, given_stun_timer = 0):
+	damage = given_damage
+	knockback_force = given_knockback_force
+	knockback_source = given_knockback_source
+	stun_timer = given_stun_timer
