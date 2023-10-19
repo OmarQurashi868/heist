2dcc86ba71ee5a436ef599dcfaf190044a62c5b4
PsychonicJoe <51345357+PsychonicJoe@users.noreply.github.com>
2023-10-20 00:24:57 +0400

    made player spawning dynamic

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -16,12 +16,15 @@ class Team:
 	func add_score():
 		self.score += 1
 
+
 @onready var money_bag: Area3D = $"../MoneyBag"
 @onready var money_bag_scene: PackedScene = preload("res://object_scenes/money_bag.tscn")
+@onready var grid_container: GridContainer = $"../GridContainer"
+@onready var sub_viewport_container: PackedScene = preload("res://object_scenes/sub_viewport_container.tscn")
 @onready var score_label_path: String = "../HBoxContainer/ScoreLabel"
 @export var player_scene: PackedScene = preload("res://object_scenes/player.tscn")
-@export_range(2,8) var players_num = 4
-@export var current_map = "LevelTest"
+@export_range(2,8) var local_players_num = 4
+@export var current_map = LobbyManager.current_map
 
 var carrier_player_id = -1
 
@@ -35,54 +38,73 @@ var teams: Array[Team] = [
 
 func _ready():
 	prepare_teams()
-	spawn_players()
 	get_tree().root.size_changed.connect(on_viewport_size_changed)
+	if local_players_num == 1:
+		grid_container.columns = 1
+	elif local_players_num == 2:
+		grid_container.columns = 2
+	else:
+		grid_container.columns = ceil(local_players_num / 2.0)
+	for i in range(local_players_num):
+		spawn_player(i, i % 4)
 	on_viewport_size_changed()
 
 
 func on_viewport_size_changed():
 	var current_resolution = DisplayServer.window_get_size()
 	var viewport_containers = get_tree().get_nodes_in_group("subviewportcontainer")
-	var resolution_x = current_resolution.x / 2.0
-	var resolution_y = current_resolution.y / ceil(players_num / 2.0)
-	var resolution_per_player = Vector2(resolution_x, resolution_y)
+	# Calculate subviewport resolution
+	var player_resolution = Vector2.ZERO
+	if local_players_num == 1:
+		player_resolution = current_resolution
+	elif local_players_num == 2:
+		player_resolution.x = current_resolution.x / 2.0
+		player_resolution.y = current_resolution.y / ceil(local_players_num / 2.0)
+	else:
+		player_resolution.x = current_resolution.x / ceil(local_players_num / 2.0)
+		player_resolution.y = current_resolution.y / 2.0
+	var resolution_per_player = player_resolution
 	
 	for container in viewport_containers:
 		container.get_node("SubViewport").size = resolution_per_player
 
 
-func spawn_players() -> void:
-	for i in range(players_num):
-		# Spawn new player and modify its properties
-		var player = player_scene.instantiate()
-		player.name += str(i)
-		player.player_id = i
-		player.team_id = i
-		player.add_to_group(teams[i].name)
-		# This function needs to be called because all of it's calls need to happen at the end
-		parent_and_adjust.call_deferred(player, teams[i].spawn_position)
-		
-		# Change the material for each player
-		var mesh: MeshInstance3D = player.get_node("MeshInstance3D")
-		var new_material = StandardMaterial3D.new()
-		new_material.albedo_color = teams[i].color
-		mesh.material_override = new_material
-		
-		# Declare and rename cameras
-		var camera_node = player.get_node("Camera3D")
-		var camera_slot = player.get_node("CameraSlot")
-		camera_node.name += str(i)
-		
-		# Reparent camera to Subviewports
-		var viewport_path = "GridContainer/SubViewportContainer" + str(i) + "/SubViewport"
-		var viewport = get_parent().get_node(viewport_path)
-		camera_node.get_parent().remove_child(camera_node)
-		viewport.add_child(camera_node)
+func spawn_player(player_id: int, team_id: int) -> void:
+	# Spawn new player and modify its properties
+	var player = player_scene.instantiate()
+	player.name += str(player_id)
+	player.player_id = player_id
+	player.team_id = team_id
+	player.add_to_group(teams[team_id].name)
+	# This function needs to be called because all of it's calls need to happen at the end
+	parent_and_adjust.call_deferred(player, teams[team_id].spawn_position)
+	
+	# Change the material for each player
+	var mesh: MeshInstance3D = player.get_node("MeshInstance3D")
+	var new_material = StandardMaterial3D.new()
+	new_material.albedo_color = teams[team_id].color
+	mesh.material_override = new_material
+	
+	# Declare and rename cameras
+	var camera_node = player.get_node("Camera3D")
+	var camera_slot = player.get_node("CameraSlot")
+	camera_node.name += str(player_id)
+	
+	# Create new subviewport
+	var new_svp_container: SubViewportContainer = sub_viewport_container.instantiate()
+	new_svp_container.name += str(player_id)
+	grid_container.add_child(new_svp_container)
+	
+	# Reparent camera to Subviewports
+	var viewport_path = "GridContainer/SubViewportContainer" + str(player_id) + "/SubViewport"
+	var viewport = get_parent().get_node(viewport_path)
+	camera_node.get_parent().remove_child(camera_node)
+	viewport.add_child(camera_node)
 
-		# Set up remote_transform
-		var remote_transform = RemoteTransform3D.new()
-		remote_transform.remote_path = camera_node.get_path()
-		camera_slot.add_child(remote_transform)
+	# Set up remote_transform
+	var remote_transform = RemoteTransform3D.new()
+	remote_transform.remote_path = camera_node.get_path()
+	camera_slot.add_child(remote_transform)
 
 
 func prepare_teams() -> void:
@@ -128,7 +150,7 @@ func parent_and_adjust(player: CharacterBody3D, team_spawn: Vector3) -> void:
 	# This was seperated because the second call needs to happen after the first one
 	# and the first one needs to be call_deffered
 	get_parent().add_child(player)
-	player.global_position = team_spawn
+	player.global_position = team_spawn + Vector3(randf() * 10, team_spawn.y, randf() * 10) 
 	
 	# Rotate  camera to the center of the map
 	player.look_at(Vector3(0, player.position.y, 0))
