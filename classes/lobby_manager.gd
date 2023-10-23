1d45492b3cd3dc9d22f8d05e659de485f9183731
PsychonicJoe <51345357+PsychonicJoe@users.noreply.github.com>
2023-10-23 14:03:03 +0400

    added background threaded loading with loading screen
    i feel like it might not be loading everything but we'll see

--- classes/lobby_manager.gd
+++ classes/lobby_manager.gd
@@ -2,26 +2,39 @@ extends Node
 
 var current_map: String = "LevelTest"
 var current_map_path: String = "res://scenes/levels/level_test.tscn"
+var requested_scene_path: String = ""
+var requested_scene: Node3D
+var current_root_node: Window
 var is_local_game = true
 var local_players_num = 1
+var load_progress: Array = [0.0]
 
 var players: Dictionary = {}
 
 
 func _ready():
-	LoadingScreen.show()
+	on_loading_start()
+
+
+func _process(_delta):
+	if ResourceLoader.load_threaded_get_status(requested_scene_path, load_progress)\
+		== ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			LoadingScreen.get_node("Panel/Panel/VBoxContainer/ProgressBar").value = load_progress[0] * 100
+			print(load_progress)
+	elif ResourceLoader.load_threaded_get_status(requested_scene_path)\
+		== ResourceLoader.THREAD_LOAD_LOADED:
+			requested_scene = ResourceLoader.load_threaded_get(requested_scene_path).instantiate()
+			current_root_node.add_child.call_deferred(requested_scene)
+			requested_scene.ready.connect(on_loading_end)
 
 
 func swap_scene(current_scene: Node, new_scene_path):
 	on_loading_start()
+	requested_scene_path = new_scene_path
+	ResourceLoader.load_threaded_request(requested_scene_path)
 	
-	var new_scene: Node = load(new_scene_path).instantiate()
-	var root_node = current_scene.get_parent()
-	
+	current_root_node = current_scene.get_parent()
 	current_scene.queue_free()
-	root_node.add_child.call_deferred(new_scene)
-	#new_scene.ready.connect(on_loading_end)
-	new_scene.tree_entered.connect(on_loading_end)
 
 
 func on_loading_start():
